bafc1bce5d2ea0fcbfb1257ed5e10d1b053f7988
GivenChauke <chaukemnashe123@gmail.com>
2023-11-03 19:11:11 +0200

    Updated Chef subclasses with comments

--- Implementation/HeadChef.h
+++ Implementation/HeadChef.h
@@ -1,11 +1,32 @@
 #ifndef HEADCHEF
 #define HEADCHEF
+
 #include "Chef.h"
 
-class HeadChef:public Chef{
-    public :
+/**
+ * @class HeadChef
+ * @brief Represents the Head Chef in the kitchen, the end of the chain.
+ */
+class HeadChef : public Chef {
+public:
+    /**
+     * @brief Cooks an order, handling tasks appropriate for the Head Chef.
+     * @param order The order to be processed.
+     * @return The processed order.
+     */
     Order* cook(Order*);
-    HeadChef(Chef*,string);//end of chain thus next here should be null
+
+    /**
+     * @brief Constructs the HeadChef, the end of the Chef chain.
+     * @param nextChef The next Chef in the chain (should be null).
+     * @param type The type of Chef (grill, fryer, or head).
+     */
+    HeadChef(Chef*, string);
+
+    /**
+     * @brief Destroys the HeadChef instance.
+     */
     ~HeadChef();
 };
+
 #endif
