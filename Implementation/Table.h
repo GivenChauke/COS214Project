4731815c3bdc5d6d4dfcdd9899bff86bfc5a3a5a
Siya2-0 <siyabongambuyisa7@gmail.com>
2023-11-03 23:54:12 +0200

    re-added Table

---
+++ Implementation/Table.h
@@ -0,0 +1,21 @@
+#ifndef TABLE_H
+#define TABLE_H
+#include <iostream>
+using namespace std;
+#include "AbstractTable.h"
+
+class Table : public AbstractTable
+{
+    private:
+        /* data */
+    public:
+        Table();
+        ~Table();
+        bool AddTable(AbstractTable* table){return false;}
+        AbstractTable* Separate(){return NULL;}
+
+};
+
+
+
+#endif
